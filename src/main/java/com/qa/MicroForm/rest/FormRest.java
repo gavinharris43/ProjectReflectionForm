de51e06b04d3be48a9fb0477cf7ff28b2f00756a
JakeTrapnell <trapnelljake@gmail.com>
2019-02-27 12:24:24 +0000

    added connection to facade

--- src/main/java/com/qa/MicroForm/rest/FormRest.java
+++ src/main/java/com/qa/MicroForm/rest/FormRest.java
@@ -1,15 +1,9 @@
 package com.qa.MicroForm.rest;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +25,10 @@ public class FormRest {
 	@Value("${queue.formQueue}")
 	private String formQueue;
 	
-	@PostMapping("${path.createForm}")
-	public ReflectionForm createForm(@RequestBody ReflectionForm reflectionForm) {
-		sendToQueue(reflectionForm);
-		return service.createForm(reflectionForm);
+	@PutMapping("${path.createForm}")
+	public String createForm(@RequestBody ReflectionForm form) {
+		sendToQueue(form);
+		return "form created successfully";
 	}
 	
 	private void sendToQueue(ReflectionForm form){
